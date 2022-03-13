078396a22a8494de1ee97e6ef710a910bbccac9d
Matheus Vieira Santos <matheus.eu.mv@gmail.com>
2022-03-13 20:47:32 -0300

    CC Finalizado

--- TRF4/project/BC.v
+++ TRF4/project/BC.v
@@ -25,45 +25,47 @@ module BC (
     assign set = (~Q[0] & ~Q[1]);
     assign rac = (~Q[0] & ~Q[1]);
 
-    assign set = (Q[1] & ~Q[0]);
+    assign dec = (Q[1] & ~Q[0]);
     assign cac = (Q[1] & ~Q[0]);
 
-    assign pronto = (Q[1] & Q[0] | ~Q[1] & ~Q[0]);
+    assign pronto = (~Q[1] & ~Q[0] | Q[1] & Q[0]);
 
     always @(posedge clk or rst) begin
-        if (rst==1) Q <= 00;
+        if (rst==1) Q <= 2'b00;
         else Q <= D0;
     end
 endmodule
 
-// module testbench;
+module testbench;
 
-// reg rst0 = 1;
-// reg clk0= 0;
-// reg zero = 0;
-// reg start = 0;
-// wire set;
-// wire rac;
-// wire dec;
-// wire cac;
-// wire pronto;
+reg rst0 = 1;
+reg clk0= 0;
+reg zero = 0;
+reg start = 0;
+wire set;
+wire rac;
+wire dec;
+wire cac;
+wire pronto;
 
-// BC BC0(rst0, clk0, start, zero, set, rac, dec, cac, pronto);
+BC BC0(rst0, clk0, start, zero, set, rac, dec, cac, pronto);
 
-// always #1 begin
-//     clk0<=~clk0;
-// end
+always #1 begin
+    clk0<=~clk0;
+end
 
-// initial begin
-//     $dumpvars;
-//     #1;
-//     rst0<=0;
-//     #1;
-//     start <=1;
-//     #10;
-//     zero <=1;
-//     #10;
-//     $finish;
-// end
+initial begin
+    $dumpvars;
+    #1;
+    rst0<=0;
+    #1;
+    start <=1;
+    #1;
+    start <=0;
+    #10;
+    zero <=1;
+    #10;
+    $finish;
+end
     
-// endmodule
+endmodule
